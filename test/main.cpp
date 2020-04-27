8a5b28136bd93cd4b7bdde6eb172064385b755a7
rxcompile <rxcompile@gmail.com>
2020-04-27 21:37:27 +0300

    Add console input, support JNZ.

--- test/main.cpp
+++ test/main.cpp
@@ -6,19 +6,44 @@ int main()
 {
     Computer computer;
     auto v = computer.memory.begin();
-    *(v++) = 0x0104;
-    *(v++) = 0x1005;
+    *(v++) = 0x0200;
+    *(v++) = 0x0aF0;
+    *(v++) = 0xFF01;
+    *(v++) = 0x01CF;
+    *(v++) = 0x0aF1;
+    *(v++) = 0xFF01;
+    *(v++) = 0x01CF;
+    *(v++) = 0x0aF2;
+    *(v++) = 0xFF01;
+    *(v++) = 0x01CF;
+    *(v++) = 0x10F0;
+    *(v++) = 0x2110;
+    *(v++) = 0x01F1;
+    *(v++) = 0x12F2;
     *(v++) = 0xFE00;
-    *(v++) = 0xFFFF;
-    *(v++) = 0x0EE0;
-    *(v++) = 0xB00F;
+    *(v++) = 0xFF00;
+    *(v++) = 0x01F1;
+    *(v++) = 0x10F2;
+    *(v++) = 0xFE00;
+    *(v++) = 0xFF00;
 
     while (!computer.isHalted())
     {
+        switch (computer.check_int())
+        {
+            case Interruptions::WAIT_INPUT :
+            {
+                uint16_t in;
+                std::cin >> in;
+                computer.memory[0xCF] = in;
+                break;
+            }
+        }
         computer.printState();
         computer.tick();
     }
 
     std::cout << "Output: " << std::dec << computer.out;
     std::cout << " 0x" << std::hex << computer.out << "\n";
+    std::cout <<std::endl;
 }
